c2431c99360fa76114c2bdfbbee405cfc1745a60
Caio27816 <31149173+Caio27816@users.noreply.github.com>
2018-12-30 21:37:09 -0200

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -8,13 +8,14 @@ exports.run = async (client, message, args) => {
   for (let i = 0; i < money.length; i++) {
       let user = client.users.get(money[i].ID.split('_')[1]).username
 
-      content += `${i+1}. ${user} ~ ${money[i].data}\n`
+      content += `${i+1}. **${user}** ~ ***${money[i].data}*** xp!\n`
   }
 
   const embed = new discord.RichEmbed()
-  .setAuthor(`${message.guild.name} - RANK!`, message.guild.iconURL)
+  .setAuthor(`${message.guild.name} - Top **Rank**!`, client.user.avatarURL)
   .setDescription(content)
-  .setColor(0x51267)
+  .setThumbnail(message.guild.iconURL)
+  .setColor("red")
 
   message.channel.send(embed)
   
