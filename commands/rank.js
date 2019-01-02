25e4951b2409d1b90f7e5a585b4aa95712117e1e
Caio27816 <31149173+Caio27816@users.noreply.github.com>
2019-01-02 17:51:20 -0200

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -2,19 +2,16 @@ const discord = require("discord.js");
 const db = require("quick.db");
 
 exports.run = async (client, message, args) => {
-  let money = db.startsWith(`xp_`, { sort: '.data'});
-  let content = "";
-  for (let i = 0; i < money.length; i++) {
-      let user = client.users.get(money[i].ID.split('_')[1]).username
 
-      content += `*${i+1}-* **${user}** ~ **${money[i].data}** xp!\n`
-  }
-
-  const embed = new discord.RichEmbed()
-  .setAuthor(`${message.guild.name} - Top Rank!`, client.user.avatarURL)
-  .setDescription(content)
-  .setThumbnail(message.guild.iconURL)
-  .setColor("red")
-
-  message.channel.send(embed)
+   
+    db.startsWith(`xp_`, { sort: ".data" }).then(resp => {
+       resp.length = 10;
+      
+      let finalOutput = "**Leaderboard** \n\n";
+      for (var i in resp) {
+       finalOutput += `${i+1}`+client.users.get(resp[i].ID.split("_")[1]).username+"- "+resp[i].data;
+      }
+      message.channel.send(finalOutput);
+  });
+  
 }
